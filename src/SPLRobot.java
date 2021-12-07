43bfb532c4637a7aef01b2a0263bd535df6afa17
Dylan Osolian <dylan.osolian@gmail.com>
2021-12-07 13:18:50 +0100

    divided into packages

---
+++ src/SPLRobot.java
@@ -0,0 +1,97 @@
+
+
+import robocode.*;
+
+import robocode.util.Utils;
+import java.awt.geom.*; // for Point2D's
+import java.lang.*; // for Double and Integer objects
+import java.util.ArrayList; // for collection of waves
+
+import Movement.IMovement;
+import Movement.MovementFactory;
+import Movement.Paint;
+import Movement.TriggerMovement;
+import Movement.WaveSurfing;
+import Targeting.ITargeting;
+import Targeting.TargetingFactory;
+import concrete.ColorRobot;
+import concrete.ConstantRotation;
+import properties.ConfigurationManager;
+
+public class SPLRobot extends AdvancedRobot {
+
+	private IMovement movement;
+	private ITargeting targeting;
+	private ConstantRotation radar;
+	private ColorRobot color;
+	private Paint paint;
+
+	public SPLRobot() {
+		ConfigurationManager c = ConfigurationManager.getInstance();
+		MovementFactory movementFactory = new MovementFactory();
+		TargetingFactory targetingFactory = new TargetingFactory();
+		
+		this.movement = movementFactory.getMovement();
+		this.targeting = targetingFactory.getTargeting();
+
+		
+	
+		if (c.getProperty("Coloring")) {
+			this.color = new ColorRobot();
+		}
+		if (c.getProperty("ConstantRotation")) {
+			this.radar = new ConstantRotation();
+		}
+		if (c.getProperty("Trigger")) {
+			this.movement = new TriggerMovement();
+		}
+		if (c.getProperty("PaintWaves")) {
+			this.paint = new Paint();
+		}
+
+	}
+
+
+	public void run() {
+
+		if (color != null) {
+			color.run(this);
+		}
+		
+		if (targeting != null) {
+			targeting.run();
+		}
+		movement.run(this);
+		radar.run(this);
+
+	}
+
+	public void onScannedRobot(ScannedRobotEvent e) {
+
+		movement.onScannedRobotMove(e, this);
+		if (targeting != null) {
+			targeting.onScannedRobotTarget(e, this);
+		}
+
+	}
+	
+	public void onHitByBulletEvent(HitByBulletEvent e) {
+		movement.onHitByBullet(e);
+	}
+	
+	public void onCustomEvent(CustomEvent e) {
+		movement.onCustomEvent(e,this);
+	}
+	
+	public void onPaint(java.awt.Graphics2D g) {
+		if (paint != null) {
+			WaveSurfing robotMovement = (WaveSurfing) movement;
+			paint.onPaint(g, robotMovement._enemyWaves, robotMovement._myLocation);
+			}
+		
+	}
+
+	
+
+
+}
